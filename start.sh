c8bf29620486d479485da64e96201e8be58c531c
TrueOsiris <tim@chaubet.be>
2020-07-30 12:52:17 +0000

    no comment

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-tail -f /dev/null
+while true
+do
+	if [[ -z $(find /mnt/cache -type f -mmin -10 2>/dev/null) ]]; 
+	then 
+		echo "Newest file is older than 10 minutes"; 
+	else 
+		echo "Newest file is younger than 10 minutes"; 
+	fi
+	sleep 1
+done
+#tail -f /dev/null
