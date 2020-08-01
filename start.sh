d470aaf5a3abf961f0b950abe50400932f4fafc5
TrueOsiris <tim@chaubet.be>
2020-08-01 13:21:48 +0000

    no comment

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ do
 	if [[ -z $(find /mnt/cache -type f -mmin -10 2>/dev/null) ]]; 
 	then 
 		echo "Newest file is older than 10 minutes. Starting move process"; 
-		d=`date +%Y-%m-%d_%H%M´
+		d=`date +%Y-%m-%d_%H%M`
 		echo "Creating folder $d"
 	else 
 		echo "Newest file is younger than 10 minutes. Doing nothing"; 
