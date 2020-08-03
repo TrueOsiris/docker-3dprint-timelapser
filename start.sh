553a21a02dd4a7945ba4b7d3a0158010bb8e92e9
TrueOsiris <tim@chaubet.be>
2020-08-03 12:28:16 +0000

    no comment

--- start.sh
+++ start.sh
@@ -4,8 +4,12 @@ do
 	if [[ -z $(find /mnt/cache -type f -mmin -10 2>/dev/null) ]]; 
 	then 
 		echo "Newest file is older than 10 minutes. Starting move process"; 
-		d=`date +%Y-%m-%d_%H%M`
-		echo "Creating folder $d"
+		d=`date +%Y-%m-%d_%H%M`;
+		echo "Creating folder /mnt/timelapse_vids/$d";
+		mkdir "/mnt/timelapse_vids/$d";
+		chown -R nobody:nogroup "/mnt/timelapse_vids/$d";
+		chmod -R 777 "/mnt/timelapse_vids/$d";
+		find /mnt/cache -type f -mmin -10 -exec mv "{}" "/mnt/timelapse_vids/$d/" \;
 	else 
 		echo "Newest file is younger than 10 minutes. Doing nothing"; 
 	fi
